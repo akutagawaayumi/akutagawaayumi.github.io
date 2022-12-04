ca570a0b1f91c95c87f51a6e951559ff3fba935e
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2022-12-04 20:37:34 +0900

    Update ityped.js

--- ityped.js
+++ ityped.js
@@ -2,7 +2,7 @@ ityped.init(document.querySelector("#ityped"), {
   strings: [
     'Count Down'
   ],
-  startDelay: 100, //アニメーション開始までの遅延、大きいほど遅れる
+  startDelay: 1, //アニメーション開始までの遅延、大きいほど遅れる
   typeSpeed: 100, //表示させるスピード、大きいほどゆっくり
   loop: true, //ループ
   backDelay: 1000, //戻る時間指定
