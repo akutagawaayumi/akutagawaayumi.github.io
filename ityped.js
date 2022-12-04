0e1729cb7098e4757e55aec0997c8253081820e0
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2022-12-04 21:51:26 +0900

    Delete ityped.js

--- ityped.js
+++
@@ -1,9 +0,0 @@
-ityped.init(document.querySelector("#ityped"), {
-  strings: [
-    'Countdown'
-  ],
-  typeSpeed:  400,            //タイピングのスピード
-  startDelay: 100,             //タイピングが始まるまでの時間
-  loop: true,                  //繰り返しの有無（ true か false で指定）
-  backDelay:  400,             //タイピングが終わって消え始めるまでの時間
-})
