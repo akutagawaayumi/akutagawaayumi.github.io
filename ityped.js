472cd4749f08676a181de42c97935549eb22d8af
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2022-12-04 21:42:20 +0900

    Update ityped.js

--- ityped.js
+++ ityped.js
@@ -5,10 +5,5 @@ ityped.init(document.querySelector("#ityped"), {
   typeSpeed:  400,            //タイピングのスピード
   startDelay: 100,             //タイピングが始まるまでの時間
   loop: true,                  //繰り返しの有無（ true か false で指定）
-  // showCursor: false,           //カーソル表示の有無（ true か false で指定）
-  // cursorChar: "|",             //カーソルとして表示させる文字を" "内に指定
-  // disableBackTyping: true,    //文字を消していく動作の有無（ true か false で指定）
-  //backSpeed:  0,              //文字が消えていくときのスピード
   backDelay:  400,             //タイピングが終わって消え始めるまでの時間
-  //placeholder: false,          //プレースホルダーでのアニメーション（trueかfalseで指定）
 })
