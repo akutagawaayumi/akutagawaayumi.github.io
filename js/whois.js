6b5179a81c364a92f71a323c19bae1bf790ff81e
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2023-10-08 12:07:14 +0900

    Update whois.js

--- js/whois.js
+++ js/whois.js
@@ -11,22 +11,26 @@ async function whoisJson(requestURL) {
 }
 
 function whois(obj) {
-  const yourname = document.querySelector('meta[name="author"]').getAttribute("content")
+  const yourname = obj.name;
+  const email = obj.email;
 
-  const author = document.querySelector('#author')
+  const author = document.querySelector('#whois span');
   author.textContent = yourname;
+  author.addEventListener('click', function () {
+    author.className = author.className === "name" ? "email" : "name";
+    author.textContent = author.textContent === yourname ? email : yourname;
+  });
 
   const date_of_birth = obj.date_of_birth;
   const datetime = obj.datetime;
 
-  let jst = new Date().toLocaleString({ timeZone: 'Asia/Tokyo' });
-  let thisYear = new Date(jst).getFullYear();
-  let thisMonth = new Date(jst).getMonth();
-  let thisDate = new Date(jst).getDate();
-  let thisAge = ((new Date(jst) - new Date(datetime)) / 31557600000).toFixed(0);
+  let thisYear = new Date().getFullYear();
+  let thisMonth = new Date().getMonth();
+  let thisDate = new Date().getDate();
+  let thisAge = ((new Date() - new Date(datetime)) / 31557600000).toFixed(0);
 
-  const birth = document.querySelector("#data time");
-  const age = document.querySelector("#age");
+  const birth = document.querySelector("#whois time");
+  const age = document.querySelector("#whois .age");
   birth.textContent = date_of_birth;
 
   const birthday = new Date(date_of_birth);
@@ -44,22 +48,22 @@ function whois(obj) {
   birth.addEventListener('click', function () {
     let thisDay
     if (week == 0) {
-      thisDay = "日曜日 Sunday"
+      thisDay = "<small>日曜日</small> Sunday"
     } else if (week == 1) {
-      thisDay = "月曜日 Monday"
+      thisDay = "<small>月曜日</small> Monday"
     } else if (week == 2) {
-      thisDay = "火曜日 Tuesday"
+      thisDay = "<small>火曜日</small> Tuesday"
     } else if (week == 3) {
-      thisDay = "水曜日 Wednesday"
+      thisDay = "<small>水曜日</small> Wednesday"
     } else if (week == 4) {
-      thisDay = "木曜日 Thursday"
+      thisDay = "<small>木曜日</small> Thursday"
     } else if (week == 5) {
-      thisDay = "金曜日 Friday"
+      thisDay = "<small>金曜日</small> Friday"
     } else if (week == 6) {
-      thisDay = "土曜日 Saturday"
+      thisDay = "<small>土曜日</small> Saturday"
     }
 
-    birth.textContent = birth.textContent === date_of_birth ? thisDay : date_of_birth;
+    birth.innerHTML = birth.innerHTML === date_of_birth ? thisDay : date_of_birth;
   })
 
   function counter() {
@@ -97,12 +101,14 @@ function whois(obj) {
 }
 
 function lifeof(obj) {
-  const contentAll = obj.contents;
+  if (obj.lifeof == true) {
+    const contentAll = obj.contents;
 
-  for (const content of contentAll) {
-    const yearOf = document.querySelector(`li[data-year="${content.year}"]`);
-    const thisLife = document.createElement('p');
-    thisLife.innerHTML = content.html;
-    yearOf.appendChild(thisLife);
+    for (const content of contentAll) {
+      const yearOf = document.querySelector(`li[data-year="${content.year}"]`);
+      const thisLife = document.createElement('p');
+      thisLife.innerHTML = content.html;
+      yearOf.appendChild(thisLife);
+    }
   }
 }
